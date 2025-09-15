bbf802568a61d5978a165eb7d6419c7c3c3df6e3
unknown <hishubh09@gmail.com>
2025-09-16 02:24:26 +0530

    update a user by ID

--- controller/userController.js
+++ controller/userController.js
@@ -39,3 +39,19 @@ export const getUserById = async (req, res) => {
     res.status(500).json({ errorMessage: error.message });
   }
 };
+
+export const updateUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    const updatedData = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedData);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
