8e731722469fdf9c881876307b479be0e5c6c8f0
unknown <hishubh09@gmail.com>
2025-09-16 02:02:05 +0530

    inserting data into database

---
+++ model/userModel.js
@@ -0,0 +1,20 @@
+// for managing the structure and managing the db
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Users", userSchema);
---
+++ routes/userRoute.js
@@ -0,0 +1,9 @@
+// defining the endpoint of the application
+import express from "express";
+import { create } from "../controller/userController.js";
+
+const route = express.Router();
+
+route.post("/user", create);
+
+export default route;
