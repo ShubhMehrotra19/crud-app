e7f494e8af92121f3b3adaa0f82423667dce8fd4
unknown <hishubh09@gmail.com>
2025-09-16 02:11:05 +0530

    get a user with a specific ID

--- controller/userController.js
+++ controller/userController.js
@@ -26,3 +26,16 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ errorMessage: error.message });
   }
 };
+
+export const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist || userExist.length === 0) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    res.status(200).json(userExist);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
